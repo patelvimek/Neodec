6369f87b963643ee63878aa1c20554d334b7817d
patelvimek <patelvimek@gmail.com>
2025-12-19 18:48:23 -0500

    setup theme and config

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function MainPage({}: { children: React.ReactNode }) {
+	return (
+		<>
+			<div className="p-14">
+				<h1> Home Page </h1>
+			</div>
+		</>
+	);
+}
